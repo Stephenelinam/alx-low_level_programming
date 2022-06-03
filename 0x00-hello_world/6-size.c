12549f18299c424a80b3dac94fc61cfd83f2e303
Stephen Abiwu <steve.abiwu111@gmail.com>
2022-06-02 23:35:39 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ long long int i3;
 char c;
 float f;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c))t
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(i2));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(i3));
